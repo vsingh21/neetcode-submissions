8c9c0fc90a06b389348404ab0c494617dbd4474b
Viraj Singh <singhviraj210806@gmail.com>
2026-09-06 20:16:18 -0500

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {i:set() for i in range(numCourses)}
+        for start, end in prerequisites:
+            adj[start].add(end)
+        print(adj)
+        visited = set()
+        def dfs(path, i):
+            if i in path:
+                return False
+            if i in visited:
+                return True
+            path.add(i)
+            for j in adj[i]:
+                if not dfs(path.copy(), j):
+                    return False
+            path.remove(i)
+            visited.add(i)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(set(), i):
+                return False
+        return True
